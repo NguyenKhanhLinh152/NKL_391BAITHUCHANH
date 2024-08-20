70b7cbcffb2c8122a1593d337499363bde24767d
Nguyễn Khánh Linh <nklinh021502@gmail.com>
2024-08-20 09:32:28 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NKL_391BAITHUCHANH.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
