9438c52107d15735e4fa65312d1b81414d1a5c51
Nguyễn Khánh Linh <nklinh021502@gmail.com>
2024-08-27 14:23:10 +0700

    Update: send and receive data for Demo

---
+++ DemoMvc/Controllers/DemoController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DemoMvc.Controllers
+{
+    [Route("[controller]")]
+    public class DemoController : Controller
+    {
+        private readonly ILogger<DemoController> _logger;
+
+        public DemoController(ILogger<DemoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Demo demo)
+        {
+             string message = "Tên: " + demo.FullName;
+            ViewBag.Message = message;
+            return View();
+        }
+
+    }
+}
---
+++ DemoMvc/Models/Demo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMvc.Models
+{
+    public class Demo
+    {
+        public string FullName { get; set; }
+    }
+}
