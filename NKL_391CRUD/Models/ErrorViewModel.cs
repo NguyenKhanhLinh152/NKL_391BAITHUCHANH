38daf1dbc5456929a3c5025e3318162ee923ea12
Nguyễn Khánh Linh <nklinh021502@gmail.com>
2024-09-17 13:19:42 +0700

    Exercise 1709

---
+++ NKL_391CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NKL_391CRUD.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ NKL_391CRUD/Models/Student.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NKL_391CRUD.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+    }
+}
